a1eb40bca1255f4bf270aeb7ba34470ed1940195
Max <130272890+UlgetMax@users.noreply.github.com>
2024-03-26 20:20:13 +0300

    Update Program.cs

--- ConsoleAppOlimpiada/Program.cs
+++ ConsoleAppOlimpiada/Program.cs
@@ -1,56 +1,79 @@
-﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-//int[] nums = new int[] { 1, 2, 3, 2, 3, 4, 3, 4, 5, 4, 5, 6, 5, 6, 7 };
+ushort[] nums = new ushort[] { 1, 2, 3, 2, 3, 4, 3, 4, 5, 4, 5, 6, 5, 6, 7 };
 
-//Console.Write("Введи число N = ");
+Console.Write("Введи число N = ");
 
-//try
-//{
-//    uint n = uint.Parse(Console.ReadLine());
+try
+{
 
-//    for (int i = 0; i < nums.Length; i++)
-//    {
-//        if (nums[i] == n)
-//        {
-//            Console.WriteLine($"Индексы значений: {i + 1} \t числа {n}");
-//        }
+    ushort n = ushort.Parse(Console.ReadLine());
+    bool B = false;
 
-//    }
-//}
-//catch
-//{
-//    Console.WriteLine("Что-то пошло не так Денис Леонтьевич!");
-//}
+    foreach (ushort x in nums)
+    {
+        if (x == n)
+        {
+            B = true;
+            break;
+        }
+    }
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (!B)
+        {
+            Console.WriteLine("Нету числа");
+            break;
+        }
+        if (n >= 1 && n <= 100)
+        {
+            if (nums[i] == n) Console.WriteLine($"Индексы значений: {i + 1} \t числа {n}");
+        }
+    }
+}
+catch
+{
+    Console.WriteLine("Что-то пошло не так Денис Леонтьевич!");
+}
+finally
+{
+    Console.WriteLine("Lesson 1 completed");
+}
 
 
 
 //Lesson 2
 
-//uint foo (uint x)
-//{
-//    uint i = 1;
-//    uint j = 9;
-//    uint res = 0;
+uint foo(uint x)
+{
+    uint i = 1;
+    uint j = 9;
+    uint res = 0;
 
-//    for (; i * j < x; j *= 10, i++) 
-//    {
-//        x -= j * i;
-//        res += j;
-//    }
-//    return res + x / i;
-//}
+    for (; i * j < x; j *= 10, i++)
+    {
+        x -= j * i;
+        res += j;
+    }
+    return res + x / i;
+}
 
-//Console.Write("введи число: ");
-//try
-//{
-//    uint x = uint.Parse(Console.ReadLine());
-
-//    Console.WriteLine(foo(x + 3));
-//}
-//catch
-//{
-//    Console.WriteLine("Ошибка");
-//}
+Console.Write("введи число: ");
+try
+{
+    uint x = uint.Parse(Console.ReadLine());
+    if (x >= 1 && x <= 10000) Console.WriteLine(foo(x + 3));
+    else Console.WriteLine("Не соотв. условию");
+}
+catch
+{
+    Console.WriteLine("Ошибка");
+}
+finally
+{
+    Console.WriteLine("Lesson 2 completed");
+}
 
 
 //string s = x.ToString();
@@ -79,6 +102,12 @@ try
     ushort yellow = 0;
     while (i > 0)
     {
+        //В теории можно использовать Switch
+        if (i > 1 && i < 100)
+        {
+            Console.WriteLine("Не соотв. условию");
+            break; // нЕ сработал почему-то
+        }
         if (j >= color.Length)
         {
             j = 0;
@@ -100,6 +129,10 @@ try
 catch
 {
     Console.WriteLine("ОШибка");
+}
+finally
+{
+    Console.WriteLine("Lesson 4 completed");
 }
 
 
